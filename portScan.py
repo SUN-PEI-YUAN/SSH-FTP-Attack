4a446e73c3290d9b01033cba43002e78266a9dcb
Mark Sun <404657164@gms.tku.edu.tw>
2017-05-16 01:48:43 +0800

    Update portScan.py
    優化輸出界面

--- portScan.py
+++ portScan.py
@@ -1,54 +1,50 @@
-#!/usr/bin/env python3.
-# coding:utf-8
-import socket
-import subprocess
-import sys
+from socket import * 
+import sys, time
 from datetime import datetime
 
-# Clear the screen
-subprocess.call('clear', shell=True)
+host = ''
+max_port = 30678
+min_port = 1
 
-# Ask for input
-remoteServer    = input("Enter a remote host to scan: ")
-remoteServerIP  = socket.gethostbyname(remoteServer)
+def scan_host(host, port, r_code = 1):
+    try:
+        s = socket(AF_INET, SOCK_STREAM)
+         
+        code = s.connect_ex((host, port))
 
-# Print a nice banner with information on which host we are about to scan
-print("-"*60)
-print("Please wait, scanning remote host", remoteServerIP)
-print("-"*60)
+        if code == 0:
+            r_code = code
+            s.close()
+    except Exception as e:
+        pass
 
-# Check what time the scan started
-t1 = datetime.now()
-
-# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
-
-# We also put in some error handling for catching errors
+    return r_code
 
 try:
-    for port in range(1,1025):  
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((remoteServerIP, port))
-        if result == 0:
-            print("Port {}: 	 Open".format(port))
-        sock.close()
-
+    host = input("[*] 輸入欲掃描之主機: ")
 except KeyboardInterrupt:
-    print("You pressed Ctrl+C")
-    sys.exit()
+    print("\n\n[*] User Requsested An Interrupt.")
+    print("[*] Application Shutting Down.")
+    sys.exit(1)
 
-except socket.gaierror:
-    print('Hostname could not be resolved. Exiting')
-    sys.exit()
+hostip = gethostbyname(host)
+print("\n[*] Host: %s IP: %s" % (host, hostip))
+print("[*] 掃描於 %s...\n" % (time.strftime("%H:%M:%S")))
+start_time = datetime.now()
 
-except socket.error:
-    print("Couldn't connect to server")
-    sys.exit()
+for port in range(min_port, max_port):
+    try:
+        response = scan_host(host, port)
 
-# Checking the time again
-t2 = datetime.now()
+        if response == 0:
+            print("[*] Port %d: Open" % port)
+    except Exception as e:
+        pass
 
-# Calculates the difference of time, to see how long it took to run the script
-total =  t2 - t1
+stop_time = datetime.now()
+totalTimeDuration = stop_time - start_time
+print("\n[*] Scanning Finished At %s ..." % (time.strftime("%H:%M:%S")))
+print("[*] Scanning Duration: %s ..." % totalTimeDuration)
+print("[*] 掃瞄完畢 !")
 
-# Printing the information to screen
-print('Scanning Completed in: ', total)
+         
